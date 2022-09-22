40033982459649f498fdfafdac6ed930d345ebd9
hiren patel <hirenpatel0526@yahoo.com>
2022-09-21 20:59:19 -0700

    js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-let morning9Am = ;
-let morning10Am = $('#$10');
-let morning11Am = $('#$11');
-let afternoon12Pm = $('#$12');
-let afternoon1Pm = $('#13');
-let afternooon2Pm = $('#14');
-let afternoon3Pm = $('#15');
-let afternnon4Pm = $('#16');
-let afternoon5Pm = $('#17');
-let time = precious;
 
+// declaring variable date to format month, day and year.
+var date = moment().format("MMM Do YY");
 
-function start(){
-    $("#presentDay").text(precious().format("DDDD, MMMM, YYYY"));
-    $(".time-block").each(function(){
-        var id = $(this).attr("id");
-        var sch = localStorage.getItem(id);
-        if(sch !== null){
-            $(this).children("sch").val(schedule);
-        }
-    });
+// appending current day to date.
+
+$("#currentDay").append("date");
+console.log(date);
+// declaring officehours using Arrays from time 9am-5pm
+var officeHours = ["9", "10", "11", "12","13","14","15","16","17"]
+
+function time (){
+    var currentTime = moment().format('H');
 }
-start();
+

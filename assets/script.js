063c233183bfc71f5cb5747704c8cf49a6af3161
hiren patel <hirenpatel0526@yahoo.com>
2022-09-23 18:03:06 -0700

    done

--- assets/script.js
+++ assets/script.js
@@ -14,27 +14,43 @@ time();
 // creating function time for background color
 function time() {
     var currentTime = moment().format('H');
+    console.log(currentTime)
     for (var i = 0; i < officeHours.length; i++) {
 
-        console.log(time)
+        let suffix;
+        
+        // ** trying to find out what the suffix should 
+        // if current time (H) is greater than 11
+        // -- suffix = pm
+        if (officeHours[i] > 11){
+            suffix = 'pm';
+        } else {
+            suffix = 'am';
+        }
+
+        // console.log(time)
+        // if current time is greater or less than the time we're checking
         if (parseInt(officeHours[i]) > currentTime) {
-            $("#" + officeHours[i].attr("style", "background-color: lightblue"));
-        } else if (parseInt(officeHours[i]) < currentTime)
-            $("#" + officeHours[i].attr("style", "background-color: lightorange"));
-    } if (parseInt(officeHours[i]) == currentTime) {
-        $("#" + officeHours[i].attr("style,", "background-color: lightred"));
+            $("#" + officeHours[i] + suffix).attr("style", "background-color: grey");
+        } else if (parseInt(officeHours[i]) < currentTime) {
+            $("#" + officeHours[i] + suffix).attr("style", "background-color: grey");
+        }
+    }
+    
+    if (parseInt(officeHours[i]) === currentTime) {
+        $("#" + officeHours[i] + suffix).attr("style", "background-color: green");
     }
 }
 
 
 // creating onclick event for save button
 
-$(",save-btn").on("click", function){
+$(".save-btn").on("click", function() {
     var officeHours = $(this).parent().attr("id");
     var textContent = $("input").val().trim();
     localStorage.setItem(officeHours,textContent);
     console.log(officeHours, textContent);
-}
+});
 
 
 // attaching individual to parent
